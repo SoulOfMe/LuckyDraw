13036565a24143e636dbd8f6606042f4cdb5f716
hujianli <hujianli222@qq.com>
2017-12-18 00:42:05 +0800

    1.添加奖品和人的Model 2.完成界面内容编写 3.完成界面滚动开始及停止的效果演示

--- Source/LuckyDraw/MainViewModel.cs
+++ Source/LuckyDraw/MainViewModel.cs
@@ -1,15 +1,150 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telerik.Windows.Controls;
 
 namespace LuckyDraw
 {
     /// <summary>
     /// 主页面的ViewModel
     /// </summary>
-    public class MainViewModel
+    public class MainViewModel : ViewModelBase
     {
+        public MainViewModel()
+        {
+            //LoadData();
+            TestData();
+        }
+
+        /// <summary>
+        /// 剩余奖品数
+        /// </summary>
+        public int PrizeCount
+        {
+            get
+            {
+                return _prizesList.Count;
+            }
+        }
+
+        /// <summary>
+        /// 剩余待抽奖人数
+        /// </summary>
+        public int PeopleCount
+        {
+            get
+            {
+                return _peopleList.Count;
+            }
+        }
+
+        #region 界面列表数据
+        private ObservableCollection<Prize> _prizesList = new ObservableCollection<Prize>();
+        private ObservableCollection<Person> _peopleList = new ObservableCollection<Person>();
+
+        /// <summary>
+        /// 剩余奖品列表
+        /// </summary>
+        public ObservableCollection<Prize> PrizesList
+        {
+            get
+            {
+                return _prizesList;
+            }
+        }
+
+        /// <summary>
+        /// 剩余抽奖人数列表
+        /// </summary>
+        public ObservableCollection<Person> PeopleList
+        {
+            get
+            {
+                return _peopleList;
+            }
+        }
+        #endregion
+
+        private void LoadData()
+        {
+            //todo:读取奖品，抽奖人，中奖列表三个csv文件，更新数据列表
+        }
+
+        private void TestData()
+        {
+            //测试数据
+            _prizesList.Clear();
+            _peopleList.Clear();
+            for (int i = 1; i <= 100; i++)
+            {
+                _prizesList.Add(new Prize()
+                {
+                    ID = i,
+                    Name = "Prize_" + i.ToString(),
+                    IsUsed = false
+                });
+            }
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                _peopleList.Add(new Person()
+                {
+                    ID = 1,
+                    Name = "Persion_" + i.ToString(),
+                    IsUsed = false
+                });
+            }
+        }
+
+        public void RefrehList()
+        {
+            //去除已经抽过的.并写入CSV文件
+            List<Prize> prizes = new List<Prize>();
+            List<Person> persons = new List<Person>();
+
+            foreach (var i in _prizesList)
+            {
+                if (i.IsUsed)
+                {
+                    prizes.Add(i);
+                }
+            }
+            foreach (var i in _peopleList)
+            {
+                if (i.IsUsed)
+                {
+                    persons.Add(i);
+                }
+            }
+
+            foreach (var i in prizes)
+            {
+                _prizesList.Remove(i);
+            }
+            foreach (var i in persons)
+            {
+                _peopleList.Remove(i);
+            }
+
+            //todo：写入csv文件记录
+        }
+    }
+
+    public class Prize
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Uri { get; set; }
+        public bool IsUsed { get; set; }
+    }
+
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public bool IsUsed { get; set; }
     }
 }
--- Source/LuckyDraw/MainWindow.xaml.cs
+++ Source/LuckyDraw/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading;
+using System.Windows;
 using System.Windows.Input;
+using System.Windows.Threading;
 using Telerik.Windows.Controls;
 
 namespace LuckyDraw
@@ -7,42 +10,99 @@ namespace LuckyDraw
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : RadWindow
     {
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = new MainViewModel();
+            _mainViewModel = new MainViewModel();
+            this.DataContext = _mainViewModel;
+
+            _timer.Interval = new TimeSpan(1000);
+            _timer.Tick += Timer_Tick;
         }
 
-        private bool _isEntrieView = false;
+        //ViewModel，存储界面信息及读写操作
+        private MainViewModel _mainViewModel;
+
+        private bool _isEntrieView = false; //全屏状态
+        private bool _isDrawing = false; //抽奖状态
+        DispatcherTimer _timer = new DispatcherTimer(); //屏幕滚动定时器
+        Random _random = new Random(); //随机数生成器
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            EntireView();
+            try
+            {
+                EntireView();
+            }
+            catch (Exception ex) { }
+
         }
 
         private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
         {
-            if (e.Key == Key.Escape)
+            try
             {
-                this.WindowState = WindowState.Normal;
-                this.WindowStyle = WindowStyle.SingleBorderWindow;
+                if (e.Key == Key.Escape)
+                {
+                    this.WindowState = WindowState.Normal;
+                    //this.WindowStyle = WindowStyle.SingleBorderWindow;
+                    this.CanClose = true;
+                    this.CanMove = true;
 
-                double left = ActualWidth / 2 - 200;
-                double top = ActualHeight / 2 - 200;
-                this.Left = left > 0 ? left : 0;
-                this.Top = top > 0 ? top : 0;
-                this.Width = 400;
-                this.Height = 400;
+                    double left = ActualWidth / 2 - 200;
+                    double top = ActualHeight / 2 - 200;
+                    this.Left = left > 0 ? left : 0;
+                    this.Top = top > 0 ? top : 0;
+                    this.Width = 400;
+                    this.Height = 400;
 
-                _isEntrieView = false;
+                    _isEntrieView = false;
+                }
+                else if (e.Key == Key.Space)
+                {
+                    if (_isDrawing)
+                    {
+                        //停止滚动
+                        _timer.Stop();
+
+                        GetDrawResult();
+                    }
+                    else
+                    {
+                        //去除已经抽过的
+                        _mainViewModel.RefrehList();
+
+                        //判断奖品数和抽奖人数目
+                        if (_mainViewModel.PrizeCount == 0)
+                        {
+                            MessageBox.Show("奖品已抽完！");
+                            return;
+                        }
+
+                        if (_mainViewModel.PeopleCount == 0)
+                        {
+                            MessageBox.Show("抽奖人不足！");
+                        }
+
+                        //开始滚动
+                        _timer.Start();
+                    }
+
+                    _isDrawing = !_isDrawing;
+                }
             }
+            catch (Exception ex) { }
         }
 
         private void Window_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            EntireView();
+            try
+            {
+                EntireView();
+            }
+            catch (Exception ex) { }
         }
 
         private void EntireView()
@@ -50,10 +110,13 @@ namespace LuckyDraw
             if (!_isEntrieView)
             {
                 //设置全屏
-                this.WindowState = WindowState.Maximized;
-                this.WindowStyle = WindowStyle.None;
+                this.WindowState = WindowState.Normal;
+                //this.WindowStyle = WindowStyle.None;
                 this.ResizeMode = ResizeMode.NoResize;
-                this.Topmost = true;
+                //this.Topmost = true;
+                this.IsTopmost = true;
+                this.CanClose = false;
+                this.CanMove = false;
 
                 this.Left = 0;
                 this.Top = 0;
@@ -65,7 +128,9 @@ namespace LuckyDraw
             else
             {
                 this.WindowState = WindowState.Normal;
-                this.WindowStyle = WindowStyle.SingleBorderWindow;
+                //this.WindowStyle = WindowStyle.SingleBorderWindow;
+                this.CanClose = true;
+                this.CanMove = true;
 
                 this.Left = ActualWidth / 2 - 200;
                 this.Top = ActualHeight / 2 - 200;
@@ -74,6 +139,86 @@ namespace LuckyDraw
 
                 _isEntrieView = false;
             }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                int rad = _random.Next(0, radCarousePrizel.Items.Count - 1);
+                radCarousePrizel.BringDataItemIntoView(radCarousePrizel.Items[rad]);
+
+                rad = _random.Next(0, radCarousePrize2.Items.Count - 1);
+                radCarousePrize2.BringDataItemIntoView(radCarousePrize2.Items[rad]);
+
+                rad = _random.Next(0, radCarousePrize3.Items.Count - 1);
+                radCarousePrize3.BringDataItemIntoView(radCarousePrize3.Items[rad]);
+
+                rad = _random.Next(0, radCarousePrize4.Items.Count - 1);
+                radCarousePrize4.BringDataItemIntoView(radCarousePrize4.Items[rad]);
+
+                rad = _random.Next(0, radCarousePrize5.Items.Count - 1);
+                radCarousePrize5.BringDataItemIntoView(radCarousePrize5.Items[rad]);
+
+                rad = _random.Next(0, radCarousePerson1.Items.Count - 1);
+                radCarousePerson1.BringDataItemIntoView(radCarousePerson1.Items[rad]);
+
+                rad = _random.Next(0, radCarousePerson2.Items.Count - 1);
+                radCarousePerson2.BringDataItemIntoView(radCarousePerson2.Items[rad]);
+
+                rad = _random.Next(0, radCarousePerson3.Items.Count - 1);
+                radCarousePerson3.BringDataItemIntoView(radCarousePerson3.Items[rad]);
+
+                rad = _random.Next(0, radCarousePerson4.Items.Count - 1);
+                radCarousePerson4.BringDataItemIntoView(radCarousePerson4.Items[rad]);
+
+                rad = _random.Next(0, radCarousePerson5.Items.Count - 1);
+                radCarousePerson5.BringDataItemIntoView(radCarousePerson5.Items[rad]);
+            }
+            catch (Exception ex) { }
+        }
+
+        //获取抽奖结果
+        private void GetDrawResult()
+        {
+            //todo:随机产生10个不重复的数字
+            int rad = _random.Next(0, radCarousePrizel.Items.Count - 1);
+            var prize1 = radCarousePrizel.Items[rad] as Prize;
+            if (prize1 != null)
+            {
+                radCarousePrizel.BringDataItemIntoView(radCarousePrizel.Items[rad]);
+                prize1.IsUsed = true;
+            }
+
+            var prize2 = radCarousePrize2.Items[rad] as Prize;
+            if (prize2 != null)
+            {
+                radCarousePrize2.BringDataItemIntoView(radCarousePrize2.Items[rad]);
+                prize2.IsUsed = true;
+            }
+
+            var prize3 = radCarousePrize3.Items[rad] as Prize;
+            if (prize3 != null)
+            {
+                radCarousePrize3.BringDataItemIntoView(radCarousePrize3.Items[rad]);
+                prize3.IsUsed = true;
+            }
+
+            var prize4 = radCarousePrize4.Items[rad] as Prize;
+            if (prize4 != null)
+            {
+                radCarousePrize4.BringDataItemIntoView(radCarousePrize4.Items[rad]);
+                prize4.IsUsed = true;
+            }
+
+            var prize5 = radCarousePrize5.Items[rad] as Prize;
+            if (prize5 != null)
+            {
+                radCarousePrize5.BringDataItemIntoView(radCarousePrize5.Items[rad]);
+                prize5.IsUsed = true;
+            }
+
+            //todo:添加其他
         }
     }
 }
